21db0de4462de17a396e09d8b14ccece30821d15
IvanBarr <ivanbar.webdev@gmail.com>
2019-04-10 17:47:42 -0700

    removed unneeded comments

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,9 @@ window.document.addEventListener('mousemove', (e) => {
   sky.style.backgroundPositionY = ((e.clientY - window.innerHeight/2)/50) + 'px';
   title.style.left = ((e.clientX - window.innerWidth/2)/15)  + 'px';
   title.style.top = ((e.clientY - window.innerHeight/2)/15) + 'px';
-  // console.log((e.offsetX) + 'and' + (e.offsetY));
 });
 
 window.document.addEventListener('scroll', (e) => {
-  // console.log(window.pageYOffset);
   mars.style.marginTop = -window.pageYOffset/4 + 'px';
   rocket.style.marginTop = -window.pageYOffset*1.5 + 'px';
 });
